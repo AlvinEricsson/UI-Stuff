1177ee78b48f0c86246a9f74c5d0929199edc285
Alvin Ericsson <alvin.eriksson@elev.ga.lbs.se>
2019-01-10 10:54:48 +0100

    UI Stuff

---
+++ Assets/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public void CoolRotate(float degrees)
+    {
+        transform.Rotate(0, 0, degrees);
+    }
+
+ 
+}
